55e544158740d614756660f94baae1c57b134b65
Marconi Soares <marconi.developer@outlook.com.br>
2023-02-22 00:12:52 -0300

    deploy 002

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------
+# Script     :
+# Descrição  :
+# Versão     : 1.0
+# Autor      : Marconi S. de Jesus <marconi.developer@outlook.com.br>
+# Licença    : GNU/GPL v3.0
+# Data       : 21/02/2023
+# ------------------------------------------------------------
+# Uso:
+# ------------------------------------------------------------
+
+cd core && daphne core.asgi:application
