2389e3d694999df9cf8f07b4c541058a9d1d3a71
Marconi Soares <marconi.developer@outlook.com.br>
2023-02-25 17:57:01 -0300

    scroll-x fix

--- core/chat/views.py
+++ core/chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from core.settings import WS_URL
+
+
 # Create your views here.
 def index(request):
-    print(WS_URL)
     return render(request, 'index.html', {'websocket_url':WS_URL})
